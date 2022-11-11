38053fff0fc7744afc71ee74368bd2b6f652e6c1
Bryan Robinson <bryanlrobinson@gmail.com>
2022-11-11 13:44:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Gets a nested ToC data structure from heading data 
+
+This is currently tested against Hygraph data, but should work for most data if you follow the following structure:
+
+```json
+[
+  {
+    "type": "heading-one", // or h1
+    "id": "some-id-here" // ID for jumplink
+    // ...additionalData
+  },
+  {
+    "type": "heading-two", // or h2
+    "id": "some-other-id-here" // ID for jumplink
+    // ...additionalData
+  }
+]
+```
+Given that structure, it will return a nested structure for frontend loops and nested lists.
+
+```json 
+[
+    {
+        "type": "heading-one",
+        "id": "some-id-here"
+        // ...additionalData,
+        "tocChildren": [
+            {
+                "type": "heading-two",
+                "id": "some-other-id-here",
+                "children": [ [Object] ],
+                "tocChildren": [],
+            }
+        ]
+    }
+]
+```
+
+## Installation
+```bash
+npm i nested-toc-json-generator
+```
+
+## Usage
+
+```js
+const generateJSON = require("nested-toc-json-generator")
+
+const data = [
+  {
+    "type": "heading-one", // or h1
+    "id": "some-id-here" // ID for jumplink
+    // ...additionalData
+  },
+  {
+    "type": "heading-two", // or h2
+    "id": "some-other-id-here" // ID for jumplink
+    // ...additionalData
+  }
+]
+
+const headerJson = generateJSON(headerJson)
+
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+function getLevel(type) {
+    // get number from either string heading-one, heading-two or string h1, h2, etc.
+    // if string has dash, split and get last item and convert string one, two, etc. to number
+    // if string has no dash, split on h and get number
+
+    if (!type) return
+    if (type.includes('-')) {
+        const split = type.split('-')
+        const level = split[split.length - 1]
+        const number = level.replace('one', 1).replace('two', 2).replace('three', 3).replace('four', 4).replace('five', 5).replace('six', 6)
+        return number
+    } else {
+        const split = type.split('h')
+        const level = split[split.length - 1]
+
+        return Number(level)
+    }
+}
+
+function generateJSON (array) {
+// INPUT: 
+/* 
+[
+  {
+    type: 'heading-one', // or h1
+    id: 'd976443c-00e7-49fe-934e-009419c0c79a' // ID for jumplink
+    // ...additionalData
+  },
+  {
+    type: 'heading-two', // or h2
+    id: 'd976443c-00e7-49fe-934e-009419c0c79a' // ID for jumplink
+    // ...additionalData
+  }
+]
+
+
+OUTPUT:
+[
+    {
+        type: 'heading-one',
+        id: '12c646c0-266f-4fb0-bb9a-977364c8dd60'
+        // ...additionalData,
+        tocChildren: [
+            {
+                type: 'heading-two',
+                children: [ [Object] ],
+                tocChildren: [],
+            }
+        ]
+    }
+]
+*/
+
+    let toc = []
+    let currentLevel = 0
+    let currentToc = toc
+    let currentTocItem = null
+
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i]
+        const level = getLevel(item.type)
+
+        if (toc.length === 0) {
+            // first item
+            currentLevel = level
+        }
+        // create array item at proper level
+        if (level > currentLevel) {
+            currentToc = currentToc[currentToc.length - 1].tocChildren
+            currentTocItem = {
+                ...item,
+                tocChildren: [],
+            }
+            currentToc.push(currentTocItem)
+        }
+        if (level === currentLevel) {
+            // add to currentToc
+            currentTocItem = {
+                ...item,
+                tocChildren: [],
+            }
+            currentToc.push(currentTocItem)
+        }
+        if (level < currentLevel) {
+            // add to parentToc
+            currentTocItem = {
+                ...item,
+                tocChildren: [],
+            }
+            currentToc.push(currentTocItem)
+        }
+        currentLevel = level
+    }
+    return toc
+}
+
+module.exports = generateJSON
